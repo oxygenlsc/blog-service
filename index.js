f57a1d87780a4e8ee0129d3a7281f96c0a7ce457
oxygenlsc <37167785+oxygenlsc@users.noreply.github.com>
2020-09-13 23:15:00 +0800

    cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser')
 const {blogRouter} = require('./routes/Blog')
 const {TagRouter} = require('./routes/tag')
 const {LoginRouter} = require('./routes/login')
@@ -11,6 +12,8 @@ app.use(express.static(staticpath))
 app.use(express.urlencoded({
     extended:true
 }))//解析body的中间件 是像地址传参那样的
+// 之前我们是前端写cookie现在我们后端写Cookie 用一个第3放库
+app.use(cookieParser());
 app.use(express.json())//解析json格式的传参的
 app.use('/app/blog',blogRouter) //基地址是、app/blog 的就交给这个路由去处理再匹配 这样页可以分模块
 app.use('/app/tag',TagRouter)
--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,11 @@ const LgS = require('../services/adminService')
 router.get('/selectAdmin', async (req,res)=>{
     const data = req.query
     const backdata = await LgS.selectAdmin(data);
+    //之前我们是前端写cookie现在我们后端写Cookie 用一个第3放库
+    res.cookie('loginSuccess',true,{
+        maxAge:3600*1000
+    })//对于pc段的浏览器会自动加入到cookie里 但是移动端我们就给个header
+    res.header('mloginSuccess','true')
     res.send(backdata);
 })
 
