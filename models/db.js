a6e700703dcfab027fdc55f4883a57a53da6c41d
lisicheng <lisicheng@ewell.cc>
2021-04-13 17:12:14 +0800

    博客更新接口get换成post

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const { Sequelize} = require('sequelize')
-let sequelize = new Sequelize('reactBlog', 'reactBlog', 'PnDnNEktnhaaAbAN', {
-    host: "47.100.23.14",
-    // host: "121.4.141.100",
+let sequelize = new Sequelize('reactblog', 'reactBlog', 'PnDnNEktnhaaAbAN', {
+    // host: "47.100.23.14",
+    host: "121.4.141.100",
     dialect:'mysql',
     // logging:null
   })
